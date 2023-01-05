b2143863d1c9f47ef6f1d796e5d1162b056155b1
Wilson Jr Manzon <man17035@byui.edu>
2023-01-05 14:53:08 +0800

    renamed script

--- scripts/portal.js
+++
@@ -1,9 +0,0 @@
-// Copyright Date
-let date = new Date();
-let year = date.getFullYear();
-document.querySelector('#copyrightYear').innerHTML = year;
-
-// Last Modified
-let currentDate = document.lastModified;
-document.querySelector('#lastModified').textContent = currentDate;
-
