151f6a8a3aecd0f8ccd6bb32bb8c3277bc0451a5
Wilson Jr Manzon <man17035@byui.edu>
2023-02-14 19:42:14 +0800

    updated js code

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,9 +1,13 @@
-const temperature=document.getElementById("temperature").textContent;
-const windSpeed=document.getElementById("wind-speed").textContent;
-const windChill=document.getElementById("wind-chill");
+const temperature = document.getElementById("temperature").textContent;
+const windSpeed = document.getElementById("wind-speed").textContent;
+const windChill = document.getElementById("wind-chill");
 
-function getWindChill(temperature, windSpeed){
-    return 35.74 + (0.6215 * temperature) - (35.75 * (windSpeed ** 0.16)) + (0.4275 * temperature * (windSpeed ** 0.16))
-};
+function getWindChill(temperature, windSpeed) {
+  return 35.74 + (0.6215 * temperature) - (35.75 * (windSpeed ** 0.16)) + (0.4275 * temperature * (windSpeed ** 0.16));
+}
 
-(temperature <= 50 && windSpeed>3) ? (windChill.textContent=`${parseInt(getWindChill(temperature, windSpeed))}°`):(windChill.textContent="N/A");
+if (temperature <= 50 && windSpeed > 3) {
+  windChill.textContent = `${parseInt(getWindChill(temperature, windSpeed))}°`;
+} else {
+  windChill.textContent = "N/A";
+}
